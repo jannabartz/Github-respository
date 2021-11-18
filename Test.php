5c94fa3c3726376289c760fdb1dafb9608b3938b
jannabartz <s4jabart@uni-trier.de>
2021-11-18 16:00:40 +0100

    start frei

--- Test.php
+++ Test.php
@@ -2,11 +2,88 @@
 <head>
     <meta charset="UTF-8">
     <title>Todos</title>
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="bootstrap" type="text/css" href="bootstrap.css">
 
 </head>
 <body>
+<header class="jumbotron"> <center> <h1>  Aufgabenplaner: Todos (Aktuelles Projekt)  </h1> </center></header>
 <div class="container-fluid"></div>
+
+<div class="col-2">
+<table>
+    <tr>
+        <a href=""> Login </a>
+    </tr>
+
+    <tr>
+        <a href=""> Projekte </a>
+    </tr>
+
+    <tr>
+        <a href=""> Aktuelles Projekt </a>
+    </tr>
+
+    <tr class="eingerückt">
+        <a href=""> Reiter </a>
+    </tr>
+
+    <tr class="eingerückt">
+        <a href=""> Aufgaben </a>
+    </tr>
+
+    <tr class="eingerückt">
+        <a href=""> Mitglieder </a>
+    </tr>
+
+</table>
+</div>
+<div class="col-10">
+    <table class="table">
+        <thead>
+        <tr>
+            <p> ToDo: </p>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <p> HTML Datei erstellen (Max Mustermann) </p>
+        </tr>
+
+        <tr>
+            <p> CSS Datei erstellen (Max Mustermann) </p>
+        </tr>
+        </tbody>
+    </table>
+    <table class="table">
+        <thead>
+        <tr>
+            <p> Erledigt: </p>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <p> PC eingeschaltet (Petra Müller) </p>
+        </tr>
+
+        <tr>
+            <p> Kaffee trinken (Petra Müller) </p>
+        </tr>
+        </tbody>
+    </table>
+    <table class="table">
+        <thead>
+        <tr>
+            <p> Verschoben: </p>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <p> Für die Uni lernen (Max Mustermann) </p>
+        </tr>
+        </tbody>
+    </table>
+
+</div>
 </body>
 </html>
 
